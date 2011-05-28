5a95df0010211cbffd2aa12eff41c258ca7b1a6f
Steve Finch <springminutes@gmail.com>
2011-05-28 07:54:55 -0400

    Added missing tests; added Custom Cow Constraint plus tests

--- grails-app/domain/com/springminutes/example/Cow.groovy
+++ grails-app/domain/com/springminutes/example/Cow.groovy
@@ -8,6 +8,9 @@ class Cow {
 
     static constraints = {
         color(blank:false, nullable: false)
-        legs(range: 0..10)
+        legs(nullable: false, validator: { Long l, Cow c ->
+            def maxLegs = (c.breed == Breed.Guernsey ? 4 : 10)
+            c.legs < 0 || c.legs > maxLegs ? "invalid.range" : null
+        })
     }
 }
--- test/unit/com/springminutes/example/CowTests.groovy
+++ test/unit/com/springminutes/example/CowTests.groovy
@@ -11,7 +11,29 @@ class CowTests extends GrailsUnitTestCase {
         super.tearDown()
     }
 
-    void testSomething() {
+    void testConstraints() {
+        mockForConstraintsTests(Cow)
 
+        // Good cow
+        def cow1 = new Cow(breed: Breed.TasmanianGrey, color: "mottled", legs: 7)
+        assertTrue cow1.validate()
+        assertEquals 0, cow1.errors.allErrors.size()
+        // Cow with unknown legs
+        def cow2 = new Cow(breed: Breed.TexasLonghorn, color: "blue")
+        assertFalse cow2.validate()
+        assertEquals "nullable", cow2.errors['legs']
+        // assertEquals cow2.errors.allErrors.contains("")
+        // Cow with blank color
+        def cow3 = new Cow(breed: Breed.TexasLonghorn, color: "", legs: 1)
+        assertFalse cow3.validate()
+        assertEquals "blank", cow3.errors['color']
+        // Custom validator
+        def cow4 = new Cow(breed: Breed.Guernsey, color: "wonky", legs: 7)
+        assertFalse cow4.validate()
+        assertEquals "invalid.range", cow4.errors['legs']
+        cow4.breed = Breed.Holstein
+        // same cow, after correcting the breed
+        assertTrue cow4.validate()
+        assertNull cow4.errors['legs']
     }
 }
